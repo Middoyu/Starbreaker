25939fcb6f39d723803c01d33e3fa448c4dce8ad
Middoyu <bradleyrodgersqa@gmail.com>
2024-09-30 10:59:24 -0400

    0.0.44
    Loading time decreased, Loading UI updated, intro fade for Tres-2B.

--- Game Folder/game_components/button_component/global_button.gd
+++ Game Folder/game_components/button_component/global_button.gd
@@ -6,6 +6,9 @@ var audio_list = ["res://Game Folder/game_assets/Menus/SFX/buttons/block.ogg", "
 @onready var default_scale = scale
 @onready var hover_scale = scale * 1.1
 
+@export_category("Customization")
+@export var scales_with_input = true
+
 func load_in():
 	for i in audio_list:
 		cache.load_in_cache(i)
@@ -14,6 +17,7 @@ func load_in():
 func _ready() -> void:
 	self.connect("focus_entered", hover_just_entered)
 	self.connect("mouse_entered", hover_just_entered)
+	self.connect("pressed", on_pressed)
 	load_in()
 
 #region Audio
@@ -27,21 +31,24 @@ func play_sound(sound):
 
 func _process(delta: float) -> void:
 	pivot_offset = size / 2
-	if is_hovered():
+	if is_hovered() and scales_with_input:
 		mouse_in()
 	else:
 		mouse_out()
 
 #region Interaction
 func mouse_in():
-	var scale_tween = create_tween().set_ease(Tween.EASE_IN_OUT)
-	scale_tween.tween_property(self, "scale", hover_scale, 0.1)
+	var scale_tween = create_tween().set_ease(Tween.EASE_IN_OUT).bind_node(self)
+	scale_tween.tween_property(self, "scale", hover_scale, 0.08)
 
 func mouse_out():
-	var scale_tween = create_tween().set_ease(Tween.EASE_IN_OUT)
-	scale_tween.tween_property(self, "scale", default_scale, 0.1)
-
+	var scale_tween = create_tween().set_ease(Tween.EASE_IN_OUT).bind_node(self)
+	scale_tween.tween_property(self, "scale", default_scale, 0.08)
 
 func hover_just_entered():
 	play_sound(audio_list[3])
+
+func on_pressed():
+	play_sound(audio_list[2])
+
 #endregion
