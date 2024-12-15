38214e5b651aca7cc48f47960bdd9d726bcfcf8b
Middoyu <bradleyrodgersqa@gmail.com>
2024-12-15 10:03:04 -0500

    0.0.53.4
    Added score display similar to the health display.
    Skeleshot now updates the score by 10 on-hit & 25 on-death.

--- Game Folder/game_components/global_components/ScoreManager/ScoreEventBus.gd
+++ Game Folder/game_components/global_components/ScoreManager/ScoreEventBus.gd
@@ -6,19 +6,22 @@ var high_score := 0
 var level_scoremanager = null
 var score_display = null
 
+signal score_update(amount)
+
 func update_score(amount):
 	add_up_to_score(amount)
+	emit_signal("score_update", amount)
 
 func passive_score_update(amount, delta):
 	if is_instance_valid(global.current_stage):
 		if global.current_stage and global.current_stage is StageBase:
 			if global.current_stage.stage_started:
-				score += amount * delta
+				update_score(amount * delta)
 
 func add_up_to_score(addition_threshold):
 	for i in range(addition_threshold):
 		score += 1
-		await get_tree().create_timer(0.01, false, true, false).timeout
+		await get_tree().create_timer(0.07, false, true, false).timeout
 		if score == addition_threshold:
 			break
 
